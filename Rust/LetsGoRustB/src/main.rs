6adf7e513629cb804d7dfd1db900125377063036
Rakib H. Hridoy <rakibhhridoy@yahoo.com>
2022-10-17 15:55:43 +0600

    3.55

--- Rust/LetsGoRustB/src/main.rs
+++ Rust/LetsGoRustB/src/main.rs
@@ -2,13 +2,21 @@ use functions::sum;
 use variables::{arguments, lp, var};
 mod functions;
 mod variables;
+use std;
 
 fn main() {
-    arguments();
-    let x = 23;
-    println!("The value of x {}", x);
+    //    arguments();
+    //    let x = 23;
+    //    println!("The value of x {}", x);
 
-    somestuff();
+    let mut arg = std::env::args();
+    let firstarg = arg.next().unwrap();
+    let key = arg.next().unwrap();
+    let value = arg.next().unwrap();
+
+    println!("The key is {}", key);
+    println!("The value is {}", value)
+    //  somestuff();
 }
 
 fn somestuff() {
